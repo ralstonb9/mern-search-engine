470c2b5def1247a7570386d0604b7b8642e4de0a
ralstonb9 <ralstonb9@gmail.com>
2023-11-27 22:17:23 -0800

    Dist

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ const startApolloServer = async () => {
   app.use('/graphql', expressMiddleware(server));
   // if we're in production, serve client/build as static assets
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.use(express.static(path.join(__dirname, '../client/dist')));
   }
   
   app.use(routes);
